78ef1d8854152fa8e004186ff41405b674d23049
Enzo Maruffa <enzomm1999@gmail.com>
2020-02-16 21:11:36 -0300

    Initial Commit

---
+++ haptic-morse-code-test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  haptic-morse-code-test
+//
+//  Created by Enzo Maruffa Moreira on 16/02/20.
+//  Copyright © 2020 Enzo Maruffa Moreira. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
